2fd70a7d7dfc63b499d9f0d55da88a897b2ec09d
f-zach <63615390+f-zach@users.noreply.github.com>
2022-01-14 16:27:19 +0100

    Revamped PRS library with class object for each sensor

--- src/PRS.cpp
+++ src/PRS.cpp
@@ -1,27 +1,131 @@
 #include "PRS.h"
 
+HoneywellPressureSensor::HoneywellPressureSensor()
+{
+}
+
+// Assigns the I2C address to the sensor
+void HoneywellPressureSensor::assign_I2C_address(int i2c_address)
+{
+    _i2c_address = i2c_address;
+}
+
+// Assigns the measurement range value to the sensor (for differential sensors only the upper limit)
+void HoneywellPressureSensor::assign_range(float range)
+{
+    _range = range;
+}
+
+// Assigns the unit to the sensor
+//    input unit    →    MILLIBAR or 0 for millibars
+//                       BAR or 1 for bars
+//                       PSI or 2 for psi
+void HoneywellPressureSensor::assign_unit(int unit)
+{
+    _unit = unit;
+}
+
+// Assigns a boolean value if the sensor is differential or not
+//    input: output_unit   →    MILLIBAR or 0 for millibars
+//                              BAR or 1 for bars
+//                              PSI or 2 for psi
+void HoneywellPressureSensor::assign_differential(bool differential)
+{
+    _differential = differential;
+}
+
+// Assigns th unit that the user wants to be output by the library
+
+void HoneywellPressureSensor::assign_output_unit(int output_unit)
+{
+    _output_unit = output_unit;
+}
+
+void HoneywellPressureSensor::assign_channel(int channel_in)
+{
+    channel = channel_in;
+}
+
+void HoneywellPressureSensor::configure_sensor(int i2c_address, float range, int unit, int differential, int output_unit, int position)
+{
+    assign_I2C_address(i2c_address);
+    assign_range(range);
+    assign_unit(unit);
+    assign_differential(differential);
+    assign_output_unit(output_unit);
+    assign_channel(position);
+}
+
+int HoneywellPressureSensor::read_data()
+{
+    Wire.requestFrom(_i2c_address, 2);
+    _data = (Wire.read() << 8 | Wire.read()) & 0x3FFF;
+
+    return _data;
+}
+
+float HoneywellPressureSensor::data2pressure()
+{
+
+    pressure = ((((_data - 1638) * (_range - (-_range * _differential))) / 13107) - (_range * _differential)) * _unitConversion[_unit][_output_unit];
+
+    return pressure;
+}
+
+float HoneywellPressureSensor::read_pressure()
+{
+    read_data();
+    data2pressure();
+
+    return pressure;
+}
+
+void HoneywellPressureSensor::display_data()
+{
+    Serial.print("I2C-Adresse: ");
+    Serial.println(_i2c_address, HEX);
+    Serial.print("Range: ");
+    Serial.println(_range);
+    Serial.print("Unit: ");
+    Serial.println(_unit);
+    Serial.print("Differential: ");
+    Serial.println(_differential);
+}
+
 PRSmodule::PRSmodule(int i2cAddress)
     : _i2cAddress(i2cAddress)
 {
-    
 }
 
-void PRSmodule::config(byte sensorPositions, float PressureRange[8], byte diff, int UnitSensor[8], int UnitReq[8], byte sensor_i2c_address[8], bool checkFault, int errLED, int busyLED)
+void PRSmodule::config(byte sensorPositions, float PressureRange[8], byte diff, int UnitSensor[8], int UnitReq[8], byte sensor_i2c_address[8], bool checkFault)
 {
-    _errLED = errLED;
-    _busyLED = busyLED;
+    /*
+    Configures the PRS module
+
+    Inputs:     sensorPositions         Byte containing the sensor positions on the module
+                                        0 → not occupied, 1 → occupied
+                                        e.g.: 10011100 → Sensor positions 3,4,5 and 8 are occupied
+                
+                PressureRange           Array with a size of eight containing the unitless value of the pressure range of the sensors (for differential the absolute amount).
+                                        The values are put into the corresponding place of the array, e.g. PressureRange[5] = 100 → The unitless pressure range of the sensor in 
+                                        position 6 (array indexing!) is 100.
+
+                diff                    Byte containing information, if the sensor in a certain slot is a differential or a absolute/relative sensor
+                                        0 → absolute/relative Sensor, 1 → differential sensor
+                                        e.g.: 00001111 → sensors 1,2,3,4 are differential
+
+                unit sensors            
+                                               
+    */
 
     _sensorCount = 0;
 
-    for(_i = 0; _i < 8; _i++)
+    // Create the sensor array containing
+    for (_i = 0; _i < 8; _i++)
     {
-        if(bitRead(sensorPositions,_i))
+        if (bitRead(sensorPositions, _i))
         {
-            _ChannelNo[_sensorCount] = _i;
-            _Sensor[_i][0] = PressureRange[_i];
-            _Sensor[_i][1] = bitRead(diff, _i);
-            _Sensor[_i][2] = UnitSensor[_i];
-            _sensor_i2c_address[_i] = sensor_i2c_address[_i];
+            _Sensor[_sensorCount].configure_sensor(sensor_i2c_address[_i], PressureRange[_i], UnitSensor[_i], bitRead(diff, _i), UnitReq[_i], _i);
 
             _sensorCount++;
         }
@@ -41,72 +145,26 @@ void PRSmodule::switchToChannel(int ChannelNo)
 
 void PRSmodule::closeAllChannels()
 {
-   Wire.beginTransmission(_i2cAddress);
+    Wire.beginTransmission(_i2cAddress);
     Wire.write(0);
     Wire.endTransmission();
 }
 
-int PRSmodule::readDataSingle(int ChannelNo)
+float PRSmodule::readPressSingle(int sensor_no)
 {
-    switchToChannel(ChannelNo);
+    switchToChannel(_Sensor[sensor_no].channel);
 
-    Wire.requestFrom(_sensor_i2c_address[_ChannelNo[ChannelNo]],2);
-    rawDataSingle = (Wire.read() << 8 | Wire.read()) & 0x3FFF;
+    PressureSingle = _Sensor[sensor_no].read_pressure();
 
-    return rawDataSingle;
-}
-
-void PRSmodule::readDataAll()
-{
-    
-    for(_i = 0; _i < _sensorCount; _i++)
-    {
-       _rawData[_i] = readDataSingle(_ChannelNo[_i]);
-       rawData[_i] = _rawData[_i];
-    }
-}
-
-float PRSmodule::convertSingle(int DataVal, float PressureRange, bool diff, int UnitSensor, int UnitReq)
-{  
-    if(!diff)
-    {
-        PressureSingle = (((DataVal - 1638) * PressureRange) / 13107) * _unitConversion[UnitSensor][UnitReq];
-    }
-
-    else if(diff)
-    {
-        
-        PressureSingle = ((((DataVal - 1638) * (PressureRange - (-PressureRange))) / 13107) - PressureRange) * _unitConversion[UnitSensor][UnitReq];
-    }
-
-    return PressureSingle;
-}
-
-void PRSmodule::convertAll()
-{
-    for(_i = 0; _i < _sensorCount; _i++)
-    {
-        
-        Pressure[_i] = convertSingle(_rawData[_i], _Sensor[_ChannelNo[_i]][0],_Sensor[_ChannelNo[_i]][1], _Sensor[_ChannelNo[_i]][2], _Sensor[_ChannelNo[_i]][3]);
-    }
-}
-
-float PRSmodule::readPressSingle(int ChannelNo)
-{
-    
-    rawDataSingle = readDataSingle(ChannelNo);
     closeAllChannels();
-
-    PressureSingle = convertSingle(rawDataSingle, _Sensor[ChannelNo][0], _Sensor[ChannelNo][1], _Sensor[ChannelNo][2], _Sensor[ChannelNo][3]);
 
     return PressureSingle;
 }
 
 void PRSmodule::readPressAll()
 {
-    readDataAll();
-    closeAllChannels();
-       
-    convertAll();
-
+    for (int i = 0; i < SensorCount; i++)
+    {
+        Pressure[i] = readPressSingle(i);
+    }
 }
--- src/PRS.h
+++ src/PRS.h
@@ -1,30 +1,55 @@
 #include <Arduino.h>
 #include <i2c_t3.h>
 
+#define MILLIBAR 0
+#define BAR 1
+#define PSI 2 
+
+class HoneywellPressureSensor
+{
+    private:
+        int _i2c_address;
+        float _range;
+        int _unit;
+        int _output_unit;
+        bool _differential;
+        int _data;
+
+    public:
+        HoneywellPressureSensor();
+        void configure_sensor(int i2c_address, float range, int unit, int differential, int output_unit, int position);
+        void assign_I2C_address(int i2c_adddress);
+        void assign_range(float range);
+        void assign_unit(int unit);
+        void assign_output_unit(int output_unit);
+        void assign_differential(bool differential);
+        void assign_channel(int channel_in);
+        int read_data();
+        float data2pressure();
+        float read_pressure();
+        void display_data();
+        int channel;
+        float pressure;  
+        const float _unitConversion[3][3] = {{1,0.001,68.947572932},{1000,1,0.0689475729},{14.503773773,0.0145037738,1}};  
+};
+
 class PRSmodule
 {
 private:
     int _i2cAddress;
-    int _errLED;
-    int _busyLED;
     int _i;
     int _sensorCount;
     int _ChannelNo[8];
-    float _Sensor[8][4];
+    HoneywellPressureSensor _Sensor[8];
     byte _channelByte;
     int _rawData[8];
-    const float _unitConversion[3][3] = {{1,0.001,68.947572932},{1000,1,0.0689475729},{14.503773773,0.0145037738,1}};
     byte _sensor_i2c_address[8] = {0,0,0,0,0,0,0,0};
 
 public:
     PRSmodule(int i2cAddress);
-    void config(byte sensorPositions, float PressureRange[8], byte diff, int UnitSensor[8], int UnitReq[8], byte sensor_i2c_address[8], bool checkFault = false, int errLED = 5, int busyLED = 6);
+    void config(byte sensorPositions, float PressureRange[8], byte diff, int UnitSensor[8], int UnitReq[8], byte sensor_i2c_address[8], bool checkFault = false);
     void switchToChannel(int ChannelNo);
     void closeAllChannels();
-    int readDataSingle(int ChannelNo);
-    void readDataAll();
-    float convertSingle(int DataVal, float PressureRange, bool diff, int UnitSensor, int UnitReq);
-    void convertAll();
     float readPressSingle(int ChannelNo);  
     void readPressAll();
     int SensorCount;
@@ -36,5 +61,7 @@ public:
     
 
 };
+
+
 
 
